9b1412c122d7b1c8e4e7428e2280de730c348f8e
Ruan Lima <93554503+Ruan12419@users.noreply.github.com>
2024-05-08 19:05:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// Conexão com o banco de dados Mongo através do mongoose
+mongoose.Promise = global.Promise;
+/*
+mongoose.connect("mongodb://localhost:27017/radios", { useNewUrlParser: true }).then( () => {
+    console.log("Database connected")},
+     err => {console.log("Can not connect to the database " + err)});
+
+     */
+
+     
+const uri = "mongodb+srv://ruanlsilva:Ruanpassword@clusterrl.ehyiwt8.mongodb.net/?retryWrites=true&w=majority&appName=ClusterRL";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
+
+
+// Importação das rotas
+const radioRoute = require("./routes/radioRoute");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/radio", radioRoute);
+
+// Rota inicial, redireciona para o arquivo HTML
+app.get("/", function(req, res){
+    res.send("Running!");
+});
+
+
+app.listen(3000, function(){
+    console.log("Listening on port 3000!");
+});
